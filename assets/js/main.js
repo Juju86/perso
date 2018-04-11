9da1e96f474e787ab983aadff47dd9027ff0b187
RHINOS\jrenou <j.renou@rhinos.fr>
2018-04-11 08:09:14 +0200

    dev header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 (function() {
   console.log($( window ));
+
   $( window ).on('scroll resize', function() {
     if( $( this ).scrollTop() > 80 ) {
       $('body').addClass('scrolled');
-      console.log($( this ).scrollTop());
     } else {
       $('body').removeClass('scrolled');
     }
   });
+
 })();

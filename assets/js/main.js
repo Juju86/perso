2cded83909fa4fe9147543653ebe7737fd65b3b5
RHINOS\jrenou <j.renou@rhinos.fr>
2018-07-18 14:17:18 +0200

    dev slider gradient

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,29 @@ $('#slider1').on('beforeChange', function(event, slick, currentSlide, nextSlide)
 $('#slider1').on('afterChange', function(event, slick, currentSlide){
   // console.log('current ' + currentSlide);
 });
+
+
+
+
+      function openCoord(e) {
+        e.style.opacity = '0'
+        e.style.display = 'none';
+        var elem = e.parentNode.parentNode;
+        elem.style.width = '380px';
+        elem.style.height = '400px';
+        var id = setTimeout(function() {
+          elem.classList.add('open')
+        }, 800);
+      }
+
+      function closeCoord(e) {
+        var elem = e.parentNode.parentNode.parentNode;
+        var btn= document.getElementById('openCoord');
+        elem.classList.remove('open');
+        elem.style.width = '50px';
+        elem.style.height = '50px';
+        var id = setTimeout(function() {
+          btn.style.opacity = '1'
+          btn.style.display = 'block';
+        }, 700);
+      }
